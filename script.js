140338540fce91c38d0ef5307bf7f29222f1e358
TottoMoe <105409104+TottoMoe@users.noreply.github.com>
2022-07-19 13:22:09 -0700

    third commit

--- script.js
+++ script.js
@@ -5,57 +5,43 @@ var formInputEl = $('.form-input');
 var save = $('.saveBtn');
 var row = $('#row');
 
-var eventEl1 = $('#event1');
-var eventEl2 = $('#event2');
-var eventEl3 = $('#event3');
-var eventEl4 = $('#event4');
-var eventEl5 = $('#event5');
-var eventEl6 = $('#event6');
-var eventEl7 = $('#event7');
-var eventEl8 = $('#event8');
-var eventEl9 = $('#event9');
-var eventEl10 = $('#event10');
-var eventEl11 = $('#event11');
-var eventEl12 = $('#event12');
-var eventEl13 = $('#event13');
-var eventEl14 = $('#event14');
-var eventEl15 = $('#event15');
-var eventEl16 = $('#event16');
-var eventEl17 = $('#event17');
-var eventEl18 = $('#event18');
-var eventEl19 = $('#event19');
-var eventEl20 = $('#event20');
-var eventEl21 = $('#event21');
-var eventEl22 = $('#event22');
-var eventEl23 = $('#event23');
-var eventEl24 = $('#event24');
+// var eventEl8 = $('#event8');
+// var eventEl9 = $('#event9');
+// var eventEl10 = $('#event10');
+// var eventEl11 = $('#event11');
+// var eventEl12 = $('#event12');
+// var eventEl13 = $('#event13');
+// var eventEl14 = $('#event14');
+// var eventEl15 = $('#event15');
+// var eventEl16 = $('#event16');
+// var eventEl17 = $('#event17');
 
-var schedule = {
-  eventEl1: $(this).text(),
-  eventEl2: $(this).text(),
-  eventEl3: $(this).text(),
-  eventEl4: $(this).text(),
-  eventEl5: $(this).text(),
-  eventEl6: $(this).text(),
-  eventEl7: $(this).text(),
-  eventEl8: $(this).text(),
-  eventEl9: $(this).text(),
-  eventEl10: $(this).text(),
-  eventEl11: $(this).text(),
-  eventEl12: $(this).text(),
-  eventEl13: $(this).text(),
-  eventEl14: $(this).text(),
-  eventEl15: $(this).text(),
-  eventEl16: $(this).text(),
-  eventEl17: $(this).text(),
-  eventEl18: $(this).text(),
-  eventEl19: $(this).text(),
-  eventEl20: $(this).text(),
-  eventEl21: $(this).text(),
-  eventEl22: $(this).text(),
-  eventEl23: $(this).text(),
-  eventEl24: $(this).text(),
-}
+// var schedule = {
+//   eventEl1: $(this).text(),
+//   eventEl2: $(this).text(),
+//   eventEl3: $(this).text(),
+//   eventEl4: $(this).text(),
+//   eventEl5: $(this).text(),
+//   eventEl6: $(this).text(),
+//   eventEl7: $(this).text(),
+//   eventEl8: $(this).text(),
+//   eventEl9: $(this).text(),
+//   eventEl10: $(this).text(),
+//   eventEl11: $(this).text(),
+//   eventEl12: $(this).text(),
+//   eventEl13: $(this).text(),
+//   eventEl14: $(this).text(),
+//   eventEl15: $(this).text(),
+//   eventEl16: $(this).text(),
+//   eventEl17: $(this).text(),
+//   eventEl18: $(this).text(),
+//   eventEl19: $(this).text(),
+//   eventEl20: $(this).text(),
+//   eventEl21: $(this).text(),
+//   eventEl22: $(this).text(),
+//   eventEl23: $(this).text(),
+//   eventEl24: $(this).text(),
+// }
 /* Display current time on the top */
 var now = moment().format('MMMM Do, YYYY h:mm:ss a');
 $('#currentDay').text(now);
@@ -72,28 +58,34 @@ save.html('<i class="fas fa-save fa-2x"></i>');
 //---------------------------------------------
 
 // Store the events
+
 table.on('input', 'td', function () {
-  var TextInput = $(this).text();
-  
+  var textInput = $(this).text();
+
   save.on('click', function() {
-    localStorage.setItem('getEvent', TextInput)
+    localStorage.setItem('getEvent', textInput)
   })
 });
 
 // Display the event items
-var item = localStorage.getItem("getEvent");
-eventEl1.text(item);
-//---------------------------------------------
-
-
-// var hours = ['1', '2', '3', '4', '5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24']
-
-
-
-var currentdate = new Date();
-var time = currentdate.getHours();
-$('.hour').each(function() {
-  $(this).text("yest" + time);
+formInputEl.each(function() {
+ 
+  var item = localStorage.getItem("getEvent");
+      $(this).text(item);
+  
 })
 
+//-----------------------------------------
 
+// Event bg change during time change
+var time = moment().hours();
+formInputEl.each(function() {
+  var num = $(this).data('event');
+  if (num > time) {
+    $(this).addClass("future");
+  } else if (time == num) {
+    $(this).addClass("present");
+  } else {
+    $(this).addClass("past");
+  }
+});
