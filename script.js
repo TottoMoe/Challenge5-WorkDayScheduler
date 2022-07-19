1c4e5da883c9e469451a060d9bf41912d949b191
TottoMoe <105409104+TottoMoe@users.noreply.github.com>
2022-07-19 14:06:19 -0700

    update README

--- script.js
+++ script.js
@@ -5,47 +5,9 @@ var formInputEl = $('.form-input');
 var save = $('.saveBtn');
 var row = $('#row');
 
-// var eventEl8 = $('#event8');
-// var eventEl9 = $('#event9');
-// var eventEl10 = $('#event10');
-// var eventEl11 = $('#event11');
-// var eventEl12 = $('#event12');
-// var eventEl13 = $('#event13');
-// var eventEl14 = $('#event14');
-// var eventEl15 = $('#event15');
-// var eventEl16 = $('#event16');
-// var eventEl17 = $('#event17');
-
-// var schedule = {
-//   eventEl1: $(this).text(),
-//   eventEl2: $(this).text(),
-//   eventEl3: $(this).text(),
-//   eventEl4: $(this).text(),
-//   eventEl5: $(this).text(),
-//   eventEl6: $(this).text(),
-//   eventEl7: $(this).text(),
-//   eventEl8: $(this).text(),
-//   eventEl9: $(this).text(),
-//   eventEl10: $(this).text(),
-//   eventEl11: $(this).text(),
-//   eventEl12: $(this).text(),
-//   eventEl13: $(this).text(),
-//   eventEl14: $(this).text(),
-//   eventEl15: $(this).text(),
-//   eventEl16: $(this).text(),
-//   eventEl17: $(this).text(),
-//   eventEl18: $(this).text(),
-//   eventEl19: $(this).text(),
-//   eventEl20: $(this).text(),
-//   eventEl21: $(this).text(),
-//   eventEl22: $(this).text(),
-//   eventEl23: $(this).text(),
-//   eventEl24: $(this).text(),
-// }
 /* Display current time on the top */
 var now = moment().format('MMMM Do, YYYY h:mm:ss a');
 $('#currentDay').text(now);
-//----------------------------------------------
 
 /* Set class for each "th" */
 tableHead.addClass('col-1 hour justify-content-center align-items-center');
@@ -55,29 +17,46 @@ formInputEl.attr('contenteditable', true);
 
 /* Add and set Save icon for each class "saveBtn" */
 save.html('<i class="fas fa-save fa-2x"></i>');
-//---------------------------------------------
 
-// Store the events
-
-table.on('input', 'td', function () {
-  var textInput = $(this).text();
+/* Store the events */
+table.on('input', function () {
+  var e8 = $("#8").text();
+  var e9 = $("#9").text();
+  var e10 = $("#10").text();
+  var e11 = $("#11").text();
+  var e12 = $("#12").text();
+  var e13 = $("#13").text();
+  var e14 = $("#14").text();
+  var e15 = $("#15").text();
+  var e16 = $("#16").text();
+  var e17 = $("#17").text();
 
   save.on('click', function() {
-    localStorage.setItem('getEvent', textInput)
+    localStorage.setItem('getEvent8', e8);
+    localStorage.setItem('getEvent9', e9);
+    localStorage.setItem('getEvent10', e10);
+    localStorage.setItem('getEvent11', e11);
+    localStorage.setItem('getEvent12', e12);
+    localStorage.setItem('getEvent13', e13);
+    localStorage.setItem('getEvent14', e14);
+    localStorage.setItem('getEvent15', e15);
+    localStorage.setItem('getEvent16', e16);
   })
 });
 
-// Display the event items
-formInputEl.each(function() {
- 
-  var item = localStorage.getItem("getEvent");
-      $(this).text(item);
-  
-})
+/* Display the event items */
+$("#8").text(localStorage.getItem("getEvent8"));
+$("#9").text(localStorage.getItem("getEvent9"));
+$("#10").text(localStorage.getItem("getEvent10"));
+$("#11").text(localStorage.getItem("getEvent11"));
+$("#12").text(localStorage.getItem("getEvent12"));
+$("#13").text(localStorage.getItem("getEvent13"));
+$("#14").text(localStorage.getItem("getEvent14"));
+$("#15").text(localStorage.getItem("getEvent15"));
+$("#16").text(localStorage.getItem("getEvent16"));
+$("#17").text(localStorage.getItem("getEvent17"));
 
-//-----------------------------------------
-
-// Event bg change during time change
+/* Event bg change during time change */
 var time = moment().hours();
 formInputEl.each(function() {
   var num = $(this).data('event');
